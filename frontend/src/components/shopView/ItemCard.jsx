65a7b7ad4f88e64cf1b57d02254aa24ee8113842
dannykhuu2 <dannykhuu2@gmail.com>
2023-03-26 01:24:20 -0400

    clean up

--- frontend/src/components/shopView/ItemCard.jsx
+++ frontend/src/components/shopView/ItemCard.jsx
@@ -3,10 +3,8 @@ import AspectRatio from "@mui/joy/AspectRatio";
 import Box from "@mui/joy/Box";
 import Button from "@mui/joy/Button";
 import Card from "@mui/joy/Card";
-import IconButton from "@mui/joy/IconButton";
 import Typography from "@mui/joy/Typography";
 import ListItem from "@mui/joy/ListItem";
-import BookmarkAdd from "@mui/icons-material/BookmarkAddOutlined";
 import { Link } from "react-router-dom";
 import { addCartAction } from "../../pages/shoppingCart/store/sliceReducer";
 
@@ -26,18 +24,6 @@ export const ItemCard = ({ item }) => {
                     </Typography>
                 </Link>
                 <Typography level="body2">Rating: {item.rating}</Typography>
-                <IconButton
-                    variant="plain"
-                    color="neutral"
-                    size="sm"
-                    sx={{
-                        position: "absolute",
-                        top: "0.5rem",
-                        right: "0.5rem",
-                    }}
-                >
-                    <BookmarkAdd />
-                </IconButton>
                 <AspectRatio minHeight="120px" maxHeight="200px" sx={{ my: 2 }}>
                     <img
                         draggable="false"
